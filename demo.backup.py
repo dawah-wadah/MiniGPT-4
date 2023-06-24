d25c8bc0329e2405cc39bd66927195752a3176fa
Wadah Adlan <wadahadlan@gmail.com>
2023-06-23 22:26:11 -0400

    readded requirements.txt

--- demo.backup.py
+++ demo.backup.py
@@ -22,10 +22,8 @@ from minigpt4.tasks import *
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Demo")
-    parser.add_argument("--cfg-path", required=True,
-                        help="path to configuration file.")
-    parser.add_argument("--gpu-id", type=int, default=0,
-                        help="specify the gpu to load the model.")
+    parser.add_argument("--cfg-path", required=True, help="path to configuration file.")
+    parser.add_argument("--gpu-id", type=int, default=0, help="specify the gpu to load the model.")
     parser.add_argument(
         "--options",
         nargs="+",
@@ -62,28 +60,20 @@ model_cls = registry.get_model_class(model_config.arch)
 model = model_cls.from_config(model_config).to('cuda:{}'.format(args.gpu_id))
 
 vis_processor_cfg = cfg.datasets_cfg.cc_sbu_align.vis_processor.train
-vis_processor = registry.get_processor_class(
-    vis_processor_cfg.name).from_config(vis_processor_cfg)
+vis_processor = registry.get_processor_class(vis_processor_cfg.name).from_config(vis_processor_cfg)
 chat = Chat(model, vis_processor, device='cuda:{}'.format(args.gpu_id))
 print('Initialization Finished')
-
-# Save the model to a file
-model_path = 'pretrained_minigpt4.pth'
-torch.save(model.state_dict(), model_path)
-print(f"Model saved to {model_path}")
 
 # ========================================
 #             Gradio Setting
 # ========================================
-
 
 def gradio_reset(chat_state, img_list):
     if chat_state is not None:
         chat_state.messages = []
     if img_list is not None:
         img_list = []
-    return None, gr.update(value=None, interactive=True), gr.update(placeholder='Please upload your image first', interactive=False), gr.update(value="Upload & Start Chat", interactive=True), chat_state, img_list
-
+    return None, gr.update(value=None, interactive=True), gr.update(placeholder='Please upload your image first', interactive=False),gr.update(value="Upload & Start Chat", interactive=True), chat_state, img_list
 
 def upload_img(gr_img, text_input, chat_state):
     if gr_img is None:
@@ -92,7 +82,6 @@ def upload_img(gr_img, text_input, chat_state):
     img_list = []
     llm_message = chat.upload_img(gr_img, chat_state, img_list)
     return gr.update(interactive=False), gr.update(interactive=True, placeholder='Type and press Enter'), gr.update(value="Start Chatting", interactive=False), chat_state, img_list
-
 
 def gradio_ask(user_message, chatbot, chat_state):
     if len(user_message) == 0:
@@ -112,13 +101,12 @@ def gradio_answer(chatbot, chat_state, img_list, num_beams, temperature):
     chatbot[-1][1] = llm_message
     return chatbot, chat_state, img_list
 
-
 title = """<h1 align="center">Demo of MiniGPT-4</h1>"""
 description = """<h3>This is the demo of MiniGPT-4. Upload your images and start chatting!</h3>"""
 article = """<p><a href='https://minigpt-4.github.io'><img src='https://img.shields.io/badge/Project-Page-Green'></a></p><p><a href='https://github.com/Vision-CAIR/MiniGPT-4'><img src='https://img.shields.io/badge/Github-Code-blue'></a></p><p><a href='https://raw.githubusercontent.com/Vision-CAIR/MiniGPT-4/main/MiniGPT_4.pdf'><img src='https://img.shields.io/badge/Paper-PDF-red'></a></p>
 """
 
-# TODO show examples below
+#TODO show examples below
 
 with gr.Blocks() as demo:
     gr.Markdown(title)
@@ -128,10 +116,9 @@ with gr.Blocks() as demo:
     with gr.Row():
         with gr.Column(scale=0.5):
             image = gr.Image(type="pil")
-            upload_button = gr.Button(
-                value="Upload & Start Chat", interactive=True, variant="primary")
+            upload_button = gr.Button(value="Upload & Start Chat", interactive=True, variant="primary")
             clear = gr.Button("Restart")
-
+            
             num_beams = gr.Slider(
                 minimum=1,
                 maximum=10,
@@ -140,7 +127,7 @@ with gr.Blocks() as demo:
                 interactive=True,
                 label="beam search numbers)",
             )
-
+            
             temperature = gr.Slider(
                 minimum=0.1,
                 maximum=2.0,
@@ -154,17 +141,13 @@ with gr.Blocks() as demo:
             chat_state = gr.State()
             img_list = gr.State()
             chatbot = gr.Chatbot(label='MiniGPT-4')
-            text_input = gr.Textbox(
-                label='User', placeholder='Please upload your image first', interactive=False)
-
-    upload_button.click(upload_img, [image, text_input, chat_state], [
-                        image, text_input, upload_button, chat_state, img_list])
-
+            text_input = gr.Textbox(label='User', placeholder='Please upload your image first', interactive=False)
+    
+    upload_button.click(upload_img, [image, text_input, chat_state], [image, text_input, upload_button, chat_state, img_list])
+    
     text_input.submit(gradio_ask, [text_input, chatbot, chat_state], [text_input, chatbot, chat_state]).then(
-        gradio_answer, [chatbot, chat_state, img_list, num_beams,
-                        temperature], [chatbot, chat_state, img_list]
+        gradio_answer, [chatbot, chat_state, img_list, num_beams, temperature], [chatbot, chat_state, img_list]
     )
-    clear.click(gradio_reset, [chat_state, img_list], [
-                chatbot, image, text_input, upload_button, chat_state, img_list], queue=False)
+    clear.click(gradio_reset, [chat_state, img_list], [chatbot, image, text_input, upload_button, chat_state, img_list], queue=False)
 
 demo.launch(share=True, enable_queue=True)
